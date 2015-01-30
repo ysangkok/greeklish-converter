1122f9940c76bf469b6d831a1f8262b68be22a1c
Janus Troelsen <ysangkok@gmail.com>
2015-01-30 01:28:05 +0100

    Update greeklish.sh

--- greeklish.sh
+++ greeklish.sh
@@ -38,7 +38,7 @@ touch __VIEWSTATE __EVENTVALIDATION __EVENTARGUMENT __EVENTTARGET
 for i in $vars ; do echo $i | cut -d"=" -f2- | tee $(echo $i | cut -d'=' -f1) > /dev/null; done
 curl -s --user-agent "$UA" --data-urlencode "G2GTextBox=$1" -d "$2Button.x=7&$2Button.y=7" --data-urlencode "__VIEWSTATE=$(cat __VIEWSTATE)" --data-urlencode "__EVENTVALIDATION=$(cat __EVENTVALIDATION)" --data-urlencode "__EVENTARGUMENT=$(cat __EVENTARGUMENT)" --data-urlencode "__EVENTTARGET=$(cat __EVENTTARGET)" "http://services.innoetics.com/gadgets/GoogleGadget/large.aspx?gid=$gid&text=$encodedurl" --trace-ascii trace -o output --speed-time 1 --speed-limit 100
 translation="$(echo $(cat output | grep "textarea" | sed -r 's/<textarea[^>]*>([^<]*)<\/textarea>/\1/gi'))"
-javascript="$(cat output | grep "kw" | cut -d';' -f 1,2)"
+javascript="$(cat output | grep "var kw" | cut -d';' -f 1,2)"
 echo $translation
 kw="$(echo $javascript | cut -d';' -f1 | cut -d'=' -f2-)"
 kw=$(node -e "JSON.stringify($kw)")
